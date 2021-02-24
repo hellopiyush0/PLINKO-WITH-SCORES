a710ed9264a1211beba56df92600d002214f4a32
hellopiyush0 <parjunlondhe0@gmail.com>
2021-02-24 19:24:17 +0530

    ok

--- js/Particle.js
+++ js/Particle.js
@@ -1,17 +1,21 @@
 class Particle {
-    constructor(x, y,r) {
 
-        var options ={
+    constructor(x, y, r) {
+
+        var options = {
+
             restitution:0.4
+
         }
-        this.r=r;
-      
-        this.body = Bodies.circle(x, y, this.r,options);       
-        this.color=color(random(0, 255), random(0, 255), random(0, 255));
+
+        this.body = Bodies.circle(x, y, 7, options);
+        this.r = 7;
+
+        this.color = color(random(0, 255), random(0, 255), random(0, 255));
         World.add(world, this.body);
 
     }
-    display() {
+    display(){
 
         var pos = this.body.position;
         var angle = this.body.angle;
@@ -19,12 +23,13 @@ class Particle {
         push();
         translate(pos.x, pos.y);
         rotate(angle);
-        //imageMode(CENTER);
         noStroke();
+
         fill(this.color)
         ellipseMode(RADIUS);
-        ellipse(0, 0, this.r,this.r);
+        ellipse(0, 0, 7, 7);
         pop();
+
     }
 
 };
--- js/Plinko.js
+++ js/Plinko.js
@@ -1,14 +1,19 @@
 class Plinko {
+
     constructor(x, y) {
+
         var options = {
+
             restitution: 1,
             friction: 0,
             isStatic:true
+
         }
+
         this.r = 10;
         this.body = Bodies.circle(x, y, this.r, options);
-        
         World.add(world, this.body);
+
     }
     display() {
 
@@ -19,11 +24,14 @@ class Plinko {
         translate(pos.x, pos.y);
         rotate(angle);
         imageMode(CENTER);
+        
         noStroke();
+
         fill("white");
         ellipseMode(RADIUS);
         ellipse(0,0,this.r,this.r);
         pop();
+
     }
 
-};
+}
